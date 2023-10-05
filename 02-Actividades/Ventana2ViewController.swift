e707fb6b75220db79d151a945e8aeff3a11e1550
alexroig <alex.roigronda@cieep.com>
2023-10-05 10:06:42 +0200

    prueba

--- 02-Actividades/Ventana2ViewController.swift
+++ 02-Actividades/Ventana2ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  Ventana2ViewController.swift
 //  02-Actividades
 //
 //  Created by Alex Roig  on 05/10/2023.
@@ -7,32 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Ventana2ViewController: UIViewController {
 
     override func viewDidLoad() {
+      
+
         super.viewDidLoad()
-       //EQUIVALENTE AL ONCREATE
-        print("Estoy en el método \(#function) de VENTANA 1")
+           //EQUIVALENTE AL ONCREATE
+        print("Estoy en el método \(#function) de VENTANA 2")
     }
+    
     //EQUIVALENTE AL ONSTART
     override func viewWillAppear(_ animated: Bool) {
-        print("Estoy  en el método \(#function) de VENTANA 1 ")
+        print("Estoy  en el método \(#function) de VENTANA 2 ")
     }
     
     //EQUIVALENTE AL ONRESUME
     override func viewDidAppear(_ animated: Bool) {
-        print("Estoy en el método \(#function) de VENTANA 1")
+        print("Estoy en el método \(#function) de VENTANA 2 ")
     }
     //EQUIVALENTE A ONSTOP
     override func viewWillDisappear(_ animated: Bool) {
-        print("Estoy en el método \(#function) de VENTANA 1")
+        print("Estoy en el método \(#function) de VENTANA 2 ")
     }
     
     //EQUIVALENTE AL ONDESTROY
     override func viewDidDisappear(_ animated: Bool) {
-        print("Estoy en el método \(#function) de VENTANA 1")
+        print("Estoy en el método \(#function) de VENTANA 2 ")
     }
 
-
 }
-
